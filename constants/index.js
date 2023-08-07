6197fe2840021570faac19d89d138b2d400b774d
fasteronomer <108072138+fasteronomer@users.noreply.github.com>
2023-08-08 01:44:58 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x8ba68b252b908667716e92a63f6e0dd37e4c5005";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
